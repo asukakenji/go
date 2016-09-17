5b96ed2f51b5fb2eff3f25c545fc828703ff1026
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2016-09-17 11:29:55 +0800

    Added more test cases

--- dsalg/avl_tree_test.go
+++ dsalg/avl_tree_test.go
@@ -117,71 +117,34 @@ func TestInsertAVL2(t *testing.T) {
 }
 
 /*
-Rotation root being left child
-------------------------------
+RR-Rotation: Rotation root being left child
+-------------------------------------------
 
-Insert: 14, 3, 16, 2, 6, 15,
+Insert: 14, 3, 16, 2, 6, 15,    Now insert: 7 (9 / 11 / 13)     Rotate with: root 3, pivot 6
         17, 1, 5, 10, 18, 4,
         8, 12
 
-        14
-     +-/ \-+
-    3       16
-   / \     / \
-  2   6   15  17
- /   / \       \
-1   5   10      18
-   /   / \
-  4   8   12
+        14                                14                                   14
+     +-/ \-+                          +--/ \--+                           +---/ \---+
+    3       16                       3         16                        6           16
+   / \     / \                     +/ \+      / \                    +--/ \--+      / \
+  2   6   15  17                  2     6    15  17                 3         10   15  17
+ /   / \       \                 /    +/ \+       \                / \     +-/ \-+      \
+1   5   10      18              1    5     10      18             2   5   8       12     18
+   /   / \                          /   +-/ \-+                  /   /   / \     / \
+  4   8   12                       4   8       12               1   4  (7) [9] [11][13]
+                                      / \     / \
+								    (7) [9] [11][13]
 
- 8: (0,0)
-10: (1,1)
- 6: (2,2)
- 3: (2,3)
-14: (4,3)
-
-Now insert: 7 (or 9 or 11 or 13)
-
-          14
-      +--/ \--+
-     3         16
-   +/ \+      / \
-  2     6    15  17
- /    +/ \+       \
-1    5     10      18
-    /   +-/ \-+
-   4   8       12
-      / \     / \
-    (7) [9] [11][13]
-
- 7: (0,0)
- 8: (0,0) -> (1,0)
-10: (1,1) -> (2,1)
- 6: (2,2) -> (2,3)
- 3: (2,3) -> (2,4) X
-14: (4,3)
-
-Rotate with: root 3, pivot 6
-
-               14
-          +---/ \---+
-         6           16
-     +--/ \--+      / \
-    3         10   15  17
-   / \     +-/ \-+      \
-  2   5   8       12     18
- /   /   / \     / \
-1   4  (7) [9] [11][13]
-
- 7: (0,0)
- 8: (0,0) -> (1,0)
-10: (1,1) -> (2,1)
- 6: (2,2) -> (2,3) -> (3,3)
- 3: (2,3) -> (2,4) -> (2,2)
-14: (4,3)
+                                 7: (0,0)                        7: (0,0)
+ 8: (0,0)                        8: (0,0) -> (1,0)               8: (0,0) -> (1,0)
+10: (1,1)                       10: (1,1) -> (2,1)              10: (1,1) -> (2,1)
+ 6: (2,2)                        6: (2,2) -> (2,3)               6: (2,2) -> (2,3) -> (3,3)
+ 3: (2,3)                        3: (2,3) -> (2,4) X             3: (2,3) -> (2,4) -> (2,2)
+14: (4,3)                       14: (4,3)                       14: (4,3)
 */
 
-func TestInsertAVLRR(t *testing.T) {
+func TestInsertAVLRRa(t *testing.T) {
 	tree := NewIntAVLTree()
 	insertMultiple(tree, 14, 3, 16, 2, 6, 15, 17, 1, 5, 10, 18, 4, 8, 12)
 	checkTraverse(t, tree, tree.TraversePreOrder, []int{14, 3, 2, 1, 6, 5, 4, 10, 8, 12, 16, 15, 17, 18})
@@ -217,4 +180,70 @@ func TestInsertAVLRR(t *testing.T) {
 	checkTraverse(t, tree5, tree5.TraversePreOrder, []int{14, 6, 3, 2, 1, 5, 4, 10, 8, 7, 9, 12, 11, 13, 16, 15, 17, 18})
 	checkTraverse(t, tree5, tree5.TraverseInOrder, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18})
 	checkTraverse(t, tree5, tree5.TraversePostOrder, []int{1, 2, 4, 5, 3, 7, 9, 8, 11, 13, 12, 10, 6, 15, 18, 17, 16, 14})
+}
+
+/*
+RR-Rotation: Rotation root being right child
+--------------------------------------------
+
+Insert: 5, 3, 8, 2, 4, 7, 11,   Now insert: 12 (14 / 16 / 18)   Rotate with: root 8, pivot 11
+        1, 6, 10, 15, 9, 13,
+        17
+
+        5                                5                                5
+     +-/ \-+                         +--/ \--+                       +---/ \---+
+    3       8                       3         8                     3           11
+   / \     / \                     / \      +/ \+                  / \      +--/ \--+
+  2   4   7   11                  2   4    7     11               2   4    8         15
+ /       /   / \                 /        /    +/ \+             /        / \     +-/ \-+
+1       6   10  15              1        6    10    15          1        7   10  13      17
+           /   / \                           /   +-/ \-+                /   /   / \     / \
+          9   13  17                        9   13      17             6   9  (12)[14][16][18]
+                                               / \     / \
+                                             (12)[14][16][18]
+
+                                12: (0,0)                       12: (0,0)
+13: (0,0)                       13: (0,0) -> (1,0)              13: (0,0) -> (1,0)
+15: (1,1)                       15: (1,1) -> (2,1)              15: (1,1) -> (2,1)
+11: (2,2)                       11: (2,2) -> (2,3)              11: (2,2) -> (2,3) -> (3,3)
+ 8: (2,3)                        8: (2,3) -> (2,4) X             8: (2,3) -> (2,4) -> (2,2)
+ 5: (3,4)                        5: (3,4)                        5: (3,4)
+*/
+
+func TestInsertAVLRRb(t *testing.T) {
+	tree := NewIntAVLTree()
+	insertMultiple(tree, 5, 3, 8, 2, 4, 7, 11, 1, 6, 10, 15, 9, 13, 17)
+	checkTraverse(t, tree, tree.TraversePreOrder, []int{5, 3, 2, 1, 4, 8, 7, 6, 11, 10, 9, 15, 13, 17})
+	checkTraverse(t, tree, tree.TraverseInOrder, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 17})
+	checkTraverse(t, tree, tree.TraversePostOrder, []int{1, 2, 4, 3, 6, 7, 9, 10, 13, 17, 15, 11, 8, 5})
+
+	tree1 := tree.Clone()
+	tree1.Insert(12)
+	checkTraverse(t, tree1, tree1.TraversePreOrder, []int{5, 3, 2, 1, 4, 11, 8, 7, 6, 10, 9, 15, 13, 12, 17})
+	checkTraverse(t, tree1, tree1.TraverseInOrder, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 17})
+	checkTraverse(t, tree1, tree1.TraversePostOrder, []int{1, 2, 4, 3, 6, 7, 9, 10, 8, 12, 13, 17, 15, 11, 5})
+
+	tree2 := tree.Clone()
+	tree2.Insert(14)
+	checkTraverse(t, tree2, tree2.TraversePreOrder, []int{5, 3, 2, 1, 4, 11, 8, 7, 6, 10, 9, 15, 13, 14, 17})
+	checkTraverse(t, tree2, tree2.TraverseInOrder, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 17})
+	checkTraverse(t, tree2, tree2.TraversePostOrder, []int{1, 2, 4, 3, 6, 7, 9, 10, 8, 14, 13, 17, 15, 11, 5})
+
+	tree3 := tree.Clone()
+	tree3.Insert(16)
+	checkTraverse(t, tree3, tree3.TraversePreOrder, []int{5, 3, 2, 1, 4, 11, 8, 7, 6, 10, 9, 15, 13, 17, 16})
+	checkTraverse(t, tree3, tree3.TraverseInOrder, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 16, 17})
+	checkTraverse(t, tree3, tree3.TraversePostOrder, []int{1, 2, 4, 3, 6, 7, 9, 10, 8, 13, 16, 17, 15, 11, 5})
+
+	tree4 := tree.Clone()
+	tree4.Insert(18)
+	checkTraverse(t, tree4, tree4.TraversePreOrder, []int{5, 3, 2, 1, 4, 11, 8, 7, 6, 10, 9, 15, 13, 17, 18})
+	checkTraverse(t, tree4, tree4.TraverseInOrder, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 17, 18})
+	checkTraverse(t, tree4, tree4.TraversePostOrder, []int{1, 2, 4, 3, 6, 7, 9, 10, 8, 13, 18, 17, 15, 11, 5})
+
+	tree5 := tree.Clone()
+	insertMultiple(tree5, 12, 14, 16, 18)
+	checkTraverse(t, tree5, tree5.TraversePreOrder, []int{5, 3, 2, 1, 4, 11, 8, 7, 6, 10, 9, 15, 13, 12, 14, 17, 16, 18})
+	checkTraverse(t, tree5, tree5.TraverseInOrder, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18})
+	checkTraverse(t, tree5, tree5.TraversePostOrder, []int{1, 2, 4, 3, 6, 7, 9, 10, 8, 12, 14, 13, 16, 18, 17, 15, 11, 5})
 }
