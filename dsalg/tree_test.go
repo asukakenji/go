80165351cdcedcb3adc907115d62763f23504386
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2016-09-18 11:27:24 +0800

    Added 'checkTraverseInt()'

--- dsalg/tree_test.go
+++ dsalg/tree_test.go
@@ -20,3 +20,15 @@ func checkTraverse(t *testing.T, tree Tree, traverseFunc func(func(interface{}))
 		panic("")
 	}
 }
+
+func checkTraverseInt(t *testing.T, tree Tree, traverseFunc func(func(interface{})), expectedStack []int) {
+	stack := make([]int, 0, tree.Len())
+	consumer := func(v interface{}) {
+		stack = append(stack, v.(int))
+	}
+	traverseFunc(consumer)
+	if !reflect.DeepEqual(stack, expectedStack) {
+		t.Errorf("tree.TraverseXXX(consumer) == %v; expected %v", stack, expectedStack)
+		panic("")
+	}
+}
