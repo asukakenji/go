c28282cbc18e63eab21ba53ce3faadf333f053b8
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2016-09-21 19:35:52 +0800

    Minor amendments

--- dsalg/tree.go
+++ dsalg/tree.go
@@ -3,6 +3,6 @@ package dsalg
 type direction *bool
 
 var (
-	left  direction = func() direction { b := false; return &b }()
-	right direction = func() direction { b := true; return &b }()
+	left  = func() direction { b := false; return &b }()
+	right = func() direction { b := true; return &b }()
 )
