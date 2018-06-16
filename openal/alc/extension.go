f3131bb37d98b712247ebd27d76ac67a54415329
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2018-06-17 00:32:11 +0800

    Added extension.go

---
+++ openal/alc/extension.go
@@ -0,0 +1,35 @@
+package alc
+
+// #cgo darwin  LDFLAGS: -framework OpenAL
+// #cgo freebsd LDFLAGS: -lopenal
+//
+// #ifdef __APPLE__
+// #include <OpenAL/al.h>
+// #include <OpenAL/alc.h>
+// #else
+// #include <AL/al.h>
+// #include <AL/alc.h>
+// #endif
+//
+// // free()
+// #include <stdlib.h>
+import "C"
+import "unsafe"
+
+func (d Device) IsExtensionPresent(extName string) bool {
+	cExtName := C.CString(extName)
+	defer C.free(unsafe.Pointer(cExtName))
+	return GoBool(C.alcIsExtensionPresent(d.CDevice(), cExtName))
+}
+
+func (d Device) GetProcAddress(funcName string) unsafe.Pointer {
+	cFuncName := C.CString(funcName)
+	defer C.free(unsafe.Pointer(cFuncName))
+	return C.alcGetProcAddress(d.CDevice(), cFuncName)
+}
+
+func (d Device) GetEnumValue(device Device, enumName string) Enum {
+	cEnumName := C.CString(enumName)
+	defer C.free(unsafe.Pointer(cEnumName))
+	return GoEnum(C.alcGetEnumValue(d.CDevice(), cEnumName))
+}
